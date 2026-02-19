8a04bbb25c135f1648858efe652a19101b0a17d3
Dave Gordon <david.gordon@nationwidechildrens.org>
2026-02-19 08:51:13 -0500

    Cleaned up pre-refactor integration tests

--- tests/integration-old/test_update_eco.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from update_source_utils import check_dynamodb_record
-from phebee.utils.aws import get_client
-
-@pytest.mark.integration
-def test_update_eco(cloudformation_stack, update_eco):
-    test_start_time = update_eco
-    check_dynamodb_record("eco", test_start_time, dynamodb=get_client("dynamodb"))
